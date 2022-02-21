7d77e381535fd1485e07c734162e8863ae10d599
cyril <cyril.cesco@gmail.com>
2022-02-21 19:41:19 +0100

    project initialisation

---
+++ src/main/java/fr/cyrilcesco/accessPass/AccessPassApplication.java
@@ -0,0 +1,13 @@
+package fr.cyrilcesco.accessPass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/cyrilcesco/accessPass/AccessPassApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cyrilcesco.accessPass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
